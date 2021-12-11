d3cd7c8d3c3de362f76decb53bc87180db9f3995
Thiago Santos da Silva <90571764+ThiagoDevSantos@users.noreply.github.com>
2021-12-11 18:28:58 -0300

    Correção de Bug

---
+++ src/main/java/br/start/localiza/model/Logo.java
@@ -0,0 +1,52 @@
+package br.start.localiza.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Logo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@OneToOne
+	@JoinColumn(name = "id_loja_loja")
+	private Loja loja;
+	
+	@Lob
+	private byte[] imagem;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Loja getLoja() {
+		return loja;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
+	}
+
+	public byte[] getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(byte[] imagem) {
+		this.imagem = imagem;
+	}
+	
+	
+
+}
